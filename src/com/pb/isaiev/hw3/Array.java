62f288ab9f365c2a5be7006d132c1053d5ae94c3
Jaroslav Isaev <jaroslav.isaev@privatbank.ua>
2021-10-23 21:11:25 +0300

    init commit

--- src/com/pb/isaiev/hw3/Array.java
+++ src/com/pb/isaiev/hw3/Array.java
@@ -3,35 +3,32 @@ import java.util.Scanner;
 
 public class Array {
     public static void main(String[] args) {
-
         Scanner scan = new Scanner(System.in);
         System.out.println("Заполните массив размерностью 10 элементов:");
-        int mas[] = new int[10];
+        int[] mas = new int[10];
         for (int i = 0; i<10; i++) {
             mas[i] = scan.nextInt();
         }
         System.out.println("Заполненый массив: ");
-        for (int i=0;i<mas.length;i++){
-            System.out.print(mas[i]+ " ");
+        for (int ma : mas) {
+            System.out.print(ma + " ");
         }
         System.out.println();
 
         int sum = 0;
-        for (int i=0;i<mas.length;i++);
         for( int num:mas) {
             sum = sum+num;
         }
         System.out.println("Сумма всех элементов массива: " + sum);
 
         int num = 0;
-        for (int i=0 ; i<mas.length; i++){
-            if (mas[i]>0 ){
+        for (int ma : mas) {
+            if (ma > 0) {
                 num++;
             }
         }
         System.out.println("Количество положительных элементов:  "+ num);
 
-        for (int i=0;i<mas.length;i++);
         boolean isSorted = false;
         int buf;
         while(!isSorted) {
